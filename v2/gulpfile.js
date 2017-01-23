9fa25601ea2919048a3e25bd1ea099151b6f3567
Matt England <m.p.england@gmail.com>
2017-01-23 22:53:25 +0000

    Changed structure for build process

--- v2/gulpfile.js
+++ v2/gulpfile.js
@@ -8,6 +8,9 @@ var uglify = require('gulp-uglify');
 var parker = require('gulp-parker');
 var browserSync = require('browser-sync').create();
 
+const gulpLoadPlugins = require('gulp-load-plugins');
+const $ = gulpLoadPlugins();
+
 // Compile sass to compressed css and add vendor prefixes
 gulp.task('styles', function() {
   gulp.src('./sass/main.scss')
@@ -44,12 +47,54 @@ gulp.task('parker', function() {
 // Static server + watching scss, js, html files
 gulp.task('serve', ['styles', 'scripts'], function() {
   browserSync.init({
-    server: '.'
+    server: './app/'
   });
-  gulp.watch('sass/*.scss', ['styles']).on('change', browserSync.reload);
-  gulp.watch('js/**/*.js', ['scripts']).on('change', browserSync.reload);
-  gulp.watch('./*.html').on('change', browserSync.reload);
+  gulp.watch('app/sass/*.scss', ['styles']).on('change', browserSync.reload);
+  gulp.watch('app/js/**/*.js', ['scripts']).on('change', browserSync.reload);
+  gulp.watch('app/*.html').on('change', browserSync.reload);
 });
+
+
+// Build
+
+gulp.task('html', ['styles', 'scripts'], () => {
+  return gulp.src('app/*.html')
+    .pipe($.useref({searchPath: ['.tmp', 'app', '.']}))
+    .pipe($.if('*.js', $.uglify()))
+    .pipe($.if('*.css', $.cssnano({safe: true, autoprefixer: false})))
+    .pipe($.if('*.html', $.htmlmin({collapseWhitespace: true})))
+    .pipe(gulp.dest('dist'));
+});
+
+gulp.task('images', () => {
+  return gulp.src('app/images/**/*')
+    .pipe($.cache($.imagemin({
+      progressive: true,
+      interlaced: true,
+      // don't remove IDs from SVGs, they are often used
+      // as hooks for embedding and styling
+      svgoPlugins: [{cleanupIDs: false}]
+    })))
+    .pipe(gulp.dest('dist/images'));
+});
+
+
+gulp.task('extras', () => {
+  return gulp.src([
+    'app/*.*',
+    '!app/*.html'
+  ], {
+    dot: true
+  }).pipe(gulp.dest('dist'));
+});
+
+
+
+
+gulp.task('build', ['html', 'images', 'extras'], () => {
+  return gulp.src('dist/**/*').pipe($.size({title: 'build', gzip: true}));
+});
+
 
 // default task
 gulp.task('default', ['serve']);
